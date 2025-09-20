343aff9ba721ddf6a4f90ee6e0096b3ce0093f32
RutikGawali1 <rutikgawali1@gmail.com>
2025-09-20 22:09:34 +0530

    terraform

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,23 +1,25 @@
+# --- Outputs ---
 output "ec2_public_ip" {
-  value = aws_instance.app_host.public_ip
+  description = "Public IP of EC2 instance"
+  value       = aws_instance.app_host.public_ip
 }
 
 output "ec2_public_dns" {
-  value = aws_instance.app_host.public_dns
-}
-
-output "jenkins_url" {
-  value = "http://${aws_instance.app_host.public_dns}:8080"
+  description = "Public DNS of EC2 instance"
+  value       = aws_instance.app_host.public_dns
 }
 
 output "app_url" {
-  value = "http://${aws_instance.app_host.public_dns}:30080"
+  description = "Wagtail CMS URL"
+  value       = "http://${aws_instance.app_host.public_dns}"
 }
 
 output "ecr_repository_url" {
-  value = aws_ecr_repository.repo.repository_url
+  description = "ECR Repository URL"
+  value       = aws_ecr_repository.repo.repository_url
 }
 
 output "s3_bucket_name" {
-  value = aws_s3_bucket.media.bucket
+  description = "S3 bucket for media storage"
+  value       = aws_s3_bucket.media.bucket
 }
